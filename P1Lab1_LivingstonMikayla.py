305ad20fbd0131f17b83217dcac726ff91854e82
xxvermxxbbxx <kaylaann808@hotmail.com>
2024-07-02 13:48:10 -0400

    Add files via upload

---
+++ P1Lab1_LivingstonMikayla.py
@@ -0,0 +1,11 @@
+
+# Mikayla Livingston
+# 6/18/2024
+# P1LAB1
+# Creating a program that allows users to enter their first name.
+
+first_name = input("Enter your first name:")
+last_name =  input("Enter your last name:")
+
+print("Hello ", first_name,last_name,"!","Welcome to CTI-110")
+ 
